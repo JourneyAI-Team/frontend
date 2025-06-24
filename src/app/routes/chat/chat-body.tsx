36e8ea5be4d55eb54519e6cf31897c9bce2ac6a1
Paula-ETG <paula.yap@everythingtogain.com>
2025-06-24 17:16:35 +0800

    feat: added toaster component for chat notifs

--- src/app/routes/chat/chat-body.tsx
+++ src/app/routes/chat/chat-body.tsx
@@ -1,9 +1,11 @@
 import type React from "react";
 import { WebSocketProvider } from "@/providers/web-sockets";
+import { Toaster } from "sonner";
 
 export const ChatBody = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex flex-col gap-5 h-full mt-10">
+      <Toaster position="top-center" richColors />
       <WebSocketProvider>{children}</WebSocketProvider>
     </main>
   );
